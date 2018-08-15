17f6127a99d0e53e36aac94b72df5ba9ba0d660f
Feixuan Li <fli@swensonhe.com>
2018-08-15 16:13:25 -0700

    init with header

---
+++ index.js
@@ -0,0 +1,11 @@
+// import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+// create a component 
+const App = () => (
+    <Header headerText={'Albums !'} />
+); 
+
+// render it to the device n
+AppRegistry.registerComponent('albums', () => App);
